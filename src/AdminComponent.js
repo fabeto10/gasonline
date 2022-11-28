aeae5d072e14e67543605554c67daea219c389e5
fabeto10 <f0414475@gmail.com>
2022-11-27 22:38:33 -0400

    firebase init copy

--- src/AdminComponent.js
+++ src/AdminComponent.js
@@ -1,6 +1,6 @@
 import React, { useState} from 'react';
 import Calendar from 'react-calendar';
-import './components/userComponent.css'
+import './components/userComponent.css';
 // let cronograma = {
 //     noviembre:{
 //         '2022': {
@@ -12,15 +12,34 @@ import './components/userComponent.css'
 const ordenPlacas = ["1 - 2", "3 - 4", "5 - 6", "7 - 8", "9 - 0"];
 export const AdminComponent = () => {
     let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-    const [ placa, setPlaca ] = useState("")
+    const [ placa, setPlaca ] = useState("");
     const [ value, onChange ] = useState(new Date());
-    function exportPlacas() {
-        let year = value.getFullYear();
-        let yearStr = toString(year);
-        let monthUpper = meses[value.getMonth()];
-        let monthLower = monthUpper.toLowerCase();
-        sessionStorage.setItem(yearStr, monthLower);
-        sessionStorage.getItem(monthLower);
+    function exportPlaca(year) {
+        let obj =  meses[value.getMonth()];
+        obj = JSON.stringify(obj);
+        sessionStorage.setItem(year, obj);
+        sessionStorage[Number(value.getFullYear())][toString(obj)] = placa;
+        console.log(sessionStorage);
+    };
+    function importPlacas(year) {
+        const value = sessionStorage.getItem(year);
+        try{
+            // console.log(JSON.parse(value.toLowerCase()));
+            const mes = JSON.parse(value.toLowerCase());
+            console.log(mes);
+            for(let i of meses){
+                let aux = [];
+                if(i === meses[value.getMonth()].toLowerCase()){
+                    const result = JSON.parse(mes);
+                    aux.push();
+                    return result;
+                };
+            };
+            console.log("error");
+            // return JSON.parse(value);
+        }catch (e) {
+            return value;
+        };
     };
     return(
         <div style={{zIndex: '0'}} >  
@@ -41,8 +60,11 @@ export const AdminComponent = () => {
                                     })
                                 }
                             </select>
-                            <button onClick={exportPlacas}>
+                            <button onClick={() => exportPlaca(value.getFullYear())}>
                                 Agregar placa
+                            </button>
+                            <button onClick={() => importPlacas(value.getFullYear())}>
+                                get placa
                             </button>
                         </div>
                         <div>
